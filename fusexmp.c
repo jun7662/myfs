d31837801e0bf0358e64ee0916378b7c2dc9ccbb
Seungjun Kim <goodkim97@gmail.com>
2017-12-15 23:37:13 +0900

    vnjv

--- fusexmp.c
+++ fusexmp.c
@@ -222,12 +222,14 @@ static int xmp_rmdir(const char *path)
 
 static int xmp_symlink(const char *from, const char *to)
 {
-  char read_fullpath[PATH_MAX];
+  char read_fullpaths[2][PATH_MAX];
   char write_fullpaths[2][PATH_MAX];
   int res;
 
-  sprintf(read_fullpath, "%s%s",
-      rand() % 2 == 0 ? global_context.driveA : global_context.driveB, from);
+ //sprintf(read_fullpath, "%s%s",rand() % 2 == 0 ? global_context.driveA : global_context.driveB, from);
+
+  sprintf(read_fullpaths[0], "%s%s", global_context.driveA, from);
+  sprintf(read_fullpaths[1], "%s%s", global_context.driveB, from);
 
   sprintf(write_fullpaths[0], "%s%s", global_context.driveA, to);
   sprintf(write_fullpaths[1], "%s%s", global_context.driveB, to);
